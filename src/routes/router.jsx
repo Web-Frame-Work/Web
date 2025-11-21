13295438b0376a3ba3ed5de40e7ecf9e3f673b48
jaehyuk <ljhsky0906@naver.com>
2025-11-21 20:34:33 +0900

    :sparkles: Feat: detail 라우터 추가

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,13 +2,14 @@ import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from '../layouts/RootLayout';
 import Home from '../pages/Home/Home';
 import NotFound from '../pages/NotFound';
+import ProductDetailPage from '../pages/Detail/ProductDetailPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <NotFound />,
-    children: [{ index: true, element: <Home /> }],
+    children: [{ index: true, element: <Home /> }, { path: '/detail/:id', element: <ProductDetailPage /> }],
   },
 ]);
 
